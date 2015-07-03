3947771e152369c38ca1f080542bdf144afdb56f
fhisa <hisa@fobj.com>
2015-07-03 10:45:36 +0900

    sync-fork-repository.rb で、作業途中でも同期できるように git stash を使うように改良

--- README.md
+++ README.md
@@ -6,12 +6,14 @@ Gitクライアントアプリ[SourceTree](https://www.sourcetreeapp.com)用の�
 
 選択したコミット(複数可)の差分を一時ファイルに出力して開く。設定の引数は $SHA
 
-
 ## open-in-terminal.sh
 
 フォルダーをターミナルで開く。Mac専用アクション。設定の引数は $REPO
 
-
 ## view-on-web.sh
 
 Webのリポジトリページを開く。[GitHub](https://github.com) と [BitBucket](https://bitbucket.org) に対応。Mac専用アクション。設定の引数は $REPO
+
+## sync-fork-repository.rb
+
+forkしたリポジトリのmasterをfork元に同期させる。
--- sync-fork-repository.rb
+++ sync-fork-repository.rb
@@ -2,9 +2,45 @@
 
 # forkしたリポジトリのmasterをfork元に同期させる。
 
+class WorkingCopyKeeper
+
+  def execute(&block)
+    @saved_branch = current_branch
+    if not system('git stash save')
+      usage 'git stash save failed.'
+      exit 1
+    end
+    begin
+      err = block.call
+      usage(err) if err
+    ensure
+      system "git checkout #{@saved_branch}"
+      system 'git stash pop'
+    end
+  end
+
+  private
+
+  def usage(msg)
+    $stderr.puts "#{File.basename($0)}: #{msg}"
+  end
+
+  def current_branch
+    pattern = /^\*\s+(.*)$/
+    `git branch`.each_line do |line|
+      if match = pattern.match(line)
+        return match[1]
+      end
+    end
+    nil
+  end
+end
+
 Dir.chdir ARGV[0] if not ARGV.empty?
-exit 1 if not system("git checkout master")
-exit 1 if not system("git fetch upstream")
-exit 1 if not system("git merge upstream/master")
-exit 1 if not system("git push origin master")
-system("git checkout mod")
+
+WorkingCopyKeeper.new.execute do
+  break 'git checkout master' if not system'git checkout master'
+  break 'git fetch upstream' if not system 'git fetch upstream'
+  break 'git merge upstream/master' if not system 'git merge upstream/master'
+  break 'git push origin master' if not system 'git push origin master'
+end
