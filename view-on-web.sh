89a3c0b691be14652d2e8707f409422f83a6c391
fhisa <hisa@fobj.com>
2016-07-06 14:48:43 +0900

    support a git protocol repository

--- view-on-web.sh
+++ view-on-web.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-chdir $@
-url=`git config --get remote.origin.url`
+sedcmd='s/^git@github\.com\:\(.*\)\.git$/https\:\/\/github.com\/\1/'
+
+cd $@
+url=`git config --get remote.origin.url | sed -e $sedcmd`
 url=`echo $url | sed 's/\.git$//'`
 url=`echo $url | sed 's/\/\/..*@/\/\//'`
 echo $url
